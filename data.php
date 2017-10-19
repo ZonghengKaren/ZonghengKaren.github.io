a238db16c7ce63906a440f37a2862dbe35850046
Zongheng <zonghengkaren@163.com>
2017-10-19 13:12:26 +0800

    10-19更新

--- data.php
+++ data.php
@@ -1,6 +1,6 @@
 <?php 
 header('Content-type:text/html; charset=utf-8');
-header("Access-Control-Allow-Origin: http://yangzongheng.cn");  
+//header("Access-Control-Allow-Origin: http://yangzongheng.cn");  
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
 header('Access-Control-Allow-Headers: x-requested-with,content-type');
